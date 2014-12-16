1c52f2cd354af104c29d976e4a371f8d6c71d568
Richard Tirta <richard.tirta@akqa.com>
2014-12-15 16:22:54 -0800

    update express settings

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
 var express = require('express');
 var port = process.env.PORT || 3000;
 var app = express();
-app.use(express.static(__dirname + ‘/dist/’));
-app.listen(port);
+
+app.use(express.static(__dirname + '/dist/'));
+
+var server = app.listen(port, function(){
+	var host = server.address().address
+  	var port = server.address().port
+	console.log('Example app listening at http://%s:%s', host, port)
+});
