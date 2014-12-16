b92190da82b6dc8b4ba63487f1d466f7798e2bb7
Richard Tirta <richard.tirta@akqa.com>
2014-12-16 11:57:32 -0800

    adding default stuff

--- src/assets/javascripts/app/main.js
+++ src/assets/javascripts/app/main.js
@@ -1,2 +1,6 @@
+//	Note that Gruntfile is being setup so it loads vendor first, everything other than main.js and main.js at the very end.
+//  Therefore make sure you put your document.ready() or any initialization here 
+//  unless you use other kind of setup such as require.js or angularj.js
+
 console.log("Halo!");
 
